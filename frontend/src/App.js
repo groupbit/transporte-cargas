2eeb7aee19d156c453291c5dfa6374b2f684760f
nataliaMarzec <https://github.com/nataliaMarzec>
2019-07-17 20:02:12 -0300

    routers agregagado

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,29 +3,34 @@ import logo from './logo.svg';
 import './App.css';
 import Clientes from "./componentes/Clientes"
 import Choferes from "./componentes/Choferes"
+import {BrowserRouter as Router, Route, Switch, Redirect, NavLink} from "react-router-dom"
+
 
 function App() {
   return (
     <div className="App">
+    <Router>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          <Clientes titulo="Clientes:" />
-        </p>
-         <p>
-          <Choferes titulo="Choferes:" />
-        </p>
-        {/* <a
-          className="AppCargas"
-          href="./Cargas"
-          rel="noopener noreferrer"
-        >
-          Ver Cargas
-         </a> */}
+      <img src={logo} className="App-logo" alt="logo" />
+        <ul>
+          
+          <li><NavLink to="/clientes">Clientes</NavLink></li>
+          <li><NavLink to="/choferes">Choferes</NavLink></li>
+        </ul>
       </header>
+      <main className="App-main">
+        <Switch>
+            <Route path="/clientes"  component={Clientes} />
+            <Route path="/choferes" component={Choferes} />
+            <Redirect to="/" />
+      </Switch>
+      </main>
+      </Router>
     </div>
   );
 }
+
+
 
 export default App;
 
--- frontend/src/componentes/Clientes.js
+++ frontend/src/componentes/Clientes.js
@@ -8,6 +8,7 @@ class Clientes extends React.Component {
     this.state= { clientes: [], seleccionado: {}}
     this.selectCliente = this.selectCliente.bind(this)
     this.clienteChangeHandler = this.clienteChangeHandler.bind(this)
+    this.addHandler = this.addHandler.bind(this)
   }
 
   selectCliente(unCliente) {
@@ -26,6 +27,23 @@ class Clientes extends React.Component {
       .then( clts => this.setState({clientes: clts}));
   }
 
+  
+  addHandler(event) {
+    fetch('http://localhost:8889/clientes', {
+        method: 'post',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(this.state.cliente)
+    }).then(res => this.props.clienteChanged(this.state.cliente) )
+      .catch(res => console.log("ERRORRRRRRRRRRR") );
+
+    event.preventDefault();
+}
+
+
+
     render() {
 
       
@@ -35,18 +53,20 @@ class Clientes extends React.Component {
               <h2>{this.props.titulo}</h2>
           
           <Table className="table">
-            <thead>
-              <tr>
+          <thead>
+          <tr>
             <th>Id</th>
             <th>Nombre</th>
             <th>Razon social</th>
-              </tr>
-            </thead>
+          </tr>
+        </thead>
             <tbody>
               {this.renderRows()}
             </tbody>
           </Table>
           <ClienteForm cliente={this.state.seleccionado} clienteChanged={this.clienteChangeHandler}/>
+          <Button><input onSubmit={this.addHandler} type="submit" value="Agregar"/></Button>
+         
         </div>)
       }
 
@@ -73,3 +93,6 @@ class Clientes extends React.Component {
 
 
   export default Clientes
+
+
+ 
